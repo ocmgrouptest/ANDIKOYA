101b4e35082f93256908b1db0ced7c85207cf4e7
AS TG[ONLINE] <126592884+TGxIRFAN@users.noreply.github.com>
2023-06-02 20:54:27 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -199,12 +199,12 @@ By @{} """
   └◌───❀*̥˚───◌───❀*̥˚┘
 
     IMDB_TEMPLATE_TXT = """
-"🎬" <b>Title : {title}</b>
-"🎭" <b>Genres : {genres}</b>
-"📆" <b>Year : {year}</b>
-"🌟" <b>Rating :  {rating}/10</b>
-"🀄" <b>Languages : {languages}</b>
-"🕒" <b>RunTime : {runtime} Minutes</b>
+<b>🎬 Title : {title}</b>
+<b>🎭 Genres : {genres}</b>
+<b>📆 Year : {year}</b>
+<b>🌟 Rating :  {rating}/10</b>
+<b>🀄 Languages : {languages}</b>
+<b>🕒 RunTime : {runtime} Minutes</b>
 
 <b>"⚡"Request by "⚡" : </b> <a href='tg://user?id={from_user.id}'><b>{}</b></a>\n
 𝙎𝙀𝙇𝙀𝘾𝙏 𝙔𝙊𝙐𝙍 𝙁𝙄𝙇𝙀 "👇"
