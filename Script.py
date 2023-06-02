c67d6fcf7199212209a0017a74ba6edcf52fe85e
ocmgrouptest <132536737+ocmgrouptest@users.noreply.github.com>
2023-06-02 20:07:55 +0530

    Update Script.py
    Fixing a error [edited]

--- Script.py
+++ Script.py
@@ -206,8 +206,8 @@ By @{} """
 '🀄' <b>Languages : {languages}</b>
 '🕒' <b>RunTime : {runtime} Minutes</b>
 
-<b>⚡️Request by⚡️ : </b> <a href='tg://user?id={from_user.id}'><b>{}</b></a>\n
-𝙎𝙀𝙇𝙀𝘾𝙏 𝙔𝙊𝙐𝙍 𝙁𝙄𝙇𝙀 👇
+<b>'⚡'Request by '⚡' : </b> <a href='tg://user?id={from_user.id}'><b>{}</b></a>\n
+𝙎𝙀𝙇𝙀𝘾𝙏 𝙔𝙊𝙐𝙍 𝙁𝙄𝙇𝙀 '👇'
 𝑻𝒊𝒑 :<b>Click Languag Button And Choose Your Languages</b> 
 
 """
