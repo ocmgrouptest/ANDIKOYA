121f5b6c1c909e7c6259abf2019538db6245923a
ocmgrouptest <132536737+ocmgrouptest@users.noreply.github.com>
2023-06-02 21:28:07 +0530

    Update Script.py
    Edited

--- Script.py
+++ Script.py
@@ -199,14 +199,14 @@ By @{} """
   └◌───❀*̥˚───◌───❀*̥˚┘
 
     IMDB_TEMPLATE_TXT = """
-<b>🎬 Title : {title}</b>
-<b>🎭 Genres : {genres}</b>
-<b>📆 Year : {year}</b>
-<b>🌟 Rating :  {rating}/10</b>
-<b>🀄 Languages : {languages}</b>
-<b>🕒 RunTime : {runtime} Minutes</b>
+🎬 Title : {title}
+🎭 Genres : {genres}
+📆 Year : {year}
+🌟 Rating :  {rating}/10
+🀄 Languages : {languages}
+🕒 RunTime : {runtime} 
 
-<b>⚡Request by⚡" : </b> <a href='tg://user?id={from_user.id}'><b>{}</b></a>\n"""
+<b>⚡Request by⚡ : </b> <a href='tg://user?id={from_user.id}'><b>{}</b></a>\n"""
 𝙎𝙀𝙇𝙀𝘾𝙏 𝙔𝙊𝙐𝙍 𝙁𝙄𝙇𝙀 "👇"
 𝑻𝒊𝒑 :<b>Click Languag Button And Choose Your Languages</b> 
 
